0983535fc36748a9f549994d6ded442514aabb04
peeranav <peeranav@scg.com>
2024-06-05 10:54:25 +0700

    Edit user_gorm_repository.go

--- internal/domain/repositories/user_gorm_repository.go
+++ internal/domain/repositories/user_gorm_repository.go
@@ -19,13 +19,13 @@ func NewUserGormRepository(db *gorm.DB) domain.IUserRepository {
 
 // Create inserts a new user record into the database
 func (r *UserGormRepository) Create(user *domain.User) error {
-	return r.db.Create(user).Error
+	return r.db.Model(&domain.User{}).Create(user).Error
 }
 
 // FindByID retrieves a user record by its ID from the database
 func (r *UserGormRepository) FindByID(id uint) (*domain.User, error) {
 	var user domain.User
-	if err := r.db.First(&user, id).Error; err != nil {
+	if err := r.db.Model(&domain.User{}).First(&user, id).Error; err != nil {
 		return nil, err
 	}
 	return &user, nil
@@ -34,7 +34,7 @@ func (r *UserGormRepository) FindByID(id uint) (*domain.User, error) {
 // FindByCredential retrieves a user record by its username and password a from the database
 func (r *UserGormRepository) FindByCredential(username string, password string) (*domain.User, error) {
 	var user domain.User
-	if err := r.db.Where(&domain.User{Username: username, PasswordHash: password, IsActive: true}).First(&user).Error; err != nil {
+	if err := r.db.Model(&domain.User{}).Where(&domain.User{Username: username, PasswordHash: password, IsActive: true}).First(&user).Error; err != nil {
 		return nil, err
 	}
 	return &user, nil
@@ -42,7 +42,7 @@ func (r *UserGormRepository) FindByCredential(username string, password string) 
 
 func (r *UserGormRepository) IsExistsByUsername(username string) (bool, error) {
 	var user *string
-	if err := r.db.Where(&domain.User{Username: username, IsActive: true}).Select("username").First(&user).Error; err != nil {
+	if err := r.db.Model(&domain.User{}).Where(&domain.User{Username: username, IsActive: true}).Select("username").First(&user).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return false, nil
 		} else {
@@ -56,10 +56,10 @@ func (r *UserGormRepository) IsExistsByUsername(username string) (bool, error) {
 
 // Update updates an existing user record in the database
 func (r *UserGormRepository) Update(user *domain.User) error {
-	return r.db.Save(user).Error
+	return r.db.Model(&domain.User{}).Save(user).Error
 }
 
 // Delete removes a user record from the database by its ID
 func (r *UserGormRepository) Delete(id uint) error {
-	return r.db.Delete(&domain.User{}, id).Error
+	return r.db.Model(&domain.User{}).Delete(&domain.User{}, id).Error
 }
